01ceb9b378a7be34c8eba7c1b53ca076a91daf4b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-24 13:33:55 +0300

    add product component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
+import { AddProduct } from "./components/AddProduct";
 export class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Routes>
           <Route exact path="/" component={Home} />
+          <Route path="/addproducts" component={AddProduct} />
         </Routes>
       </BrowserRouter>
     );

3ada004393750ec387083261f21c842098f7a0a8
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-24 13:43:20 +0300

    updating home compoenent

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,26 @@
-import React from "react";
-import '../css/Home.css'
-import { Navbar } from "./Navbar";
-import {Product} from "./Product"
-export const Home = () => {
-  return (
-    <div className='wrapper'>
-      <Navbar />
-      <Product/>
-    </div>
-  );
-};
+import React, { useEffect } from 'react'
+import { Navbar } from './Navbar';
+import { Products } from './Products'
+import { useHistory } from 'react-router-dom'
+import { auth } from '../Config/Config'
+
+export const Home = ({ user }) => {
+
+    const history = useHistory();
+
+    useEffect(() => {
+        // forcing user to signup
+        auth.onAuthStateChanged(user => {
+            if (!user) {
+                history.push('/login');
+            }
+        })
+    })
+
+    return (
+        <div className='wrapper'>
+            <Navbar user={user} />
+            <Products />
+        </div>
+    )
+}
